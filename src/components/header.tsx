dea0b3ab1ef196d5935eccce3c37435d6fad9800
Symon <symonrahman20@gmail.com>
2024-06-02 19:44:51 +1000

    Make header a static component rather than a dynamic component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,52 +1,10 @@
 import Link from "next/link";
-import { Navbar,NavbarBrand,NavbarContent,NavbarItem,Input,Button, Avatar,Popover,PopoverTrigger,PopoverContent } from "@nextui-org/react";
-import { auth } from "@/auth";
-import * as actions from "@/actions";
+import { Navbar,NavbarBrand,NavbarContent,NavbarItem,Input } from "@nextui-org/react";
+import HeaderAuth from "./header-auth";
+const Header = ()=>{
+   
 
-const Header = async()=>{
-    const session = await auth();
-
-    let authContent:React.ReactNode;
-
-    if(session?.user){
-        authContent = 
-        <Popover placement="left">
-            <PopoverTrigger>
-            <Avatar src={session.user.image|| ''}/>
-            </PopoverTrigger>
-            <PopoverContent>
-                <div className="p-4">
-                    <form action={actions.signOut}>
-                        <Button type="submit">SingOut</Button>
-                    </form>
-
-                </div>
-
-            </PopoverContent>
- 
-        </Popover>
-      
-    }else{
-        authContent = <>
-        <NavbarItem>
-            <form action={actions.signIn}>
-            <Button type="submit" color="secondary"
-            variant="bordered">
-                Sing In
-            </Button>
-            </form>
-        </NavbarItem>
-        <NavbarItem>
-            <form action={actions.signOut}>
-            <Button type="submit" color="primary"variant="flat">
-                Sing Up
-            </Button>
-            </form>
-        </NavbarItem>
-        </>
-
-    }
-
+   
     return(
         <Navbar className="shadow mb-6">
             <NavbarBrand>
@@ -60,9 +18,7 @@ const Header = async()=>{
 
             <NavbarContent justify="end">
                 <NavbarItem>
-                    {
-                       authContent
-                    }
+                    <HeaderAuth/>
                 </NavbarItem>
 
             </NavbarContent>
