7261fecc7e340816b9bd936467c8cca06afc2b9e
Symon <symonrahman20@gmail.com>
2024-06-03 18:54:30 +1000

    Using useFormSate for our creating topic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,41 @@
 "use server"
+import {z} from "zod";
 
-export async function createTopic(formData:FormData){
-   // revalidate the home page
-   const name = formData.get('name')
-   const description = formData.get('description')
+const createTopicSchema = z.object({
+   name:z.string().min(3).regex(/^[a-z-]+$/,{message:"Must be lower case letters or dashes without spaces."}),
+   description:z.string().min(10),
+});
+
+
+// this type has to match whatver you supply in your useFormState.
+// see topic-create-form.tsx the intial state.
+interface CreateFormState{
+   errors:{
+      name?:string[],
+      description?:string[]
+   }
+}
+
+/// This retunrs a Promise of type CreateFormState otherwise typescript complains about it a lot. 
+
+// see the return statements 
+export async function createTopic(formState:CreateFormState ,formData:FormData):Promise<CreateFormState>{
+
+   const result =  createTopicSchema.safeParse({
+      name:formData.get('name'),
+      description:formData.get('description')
+   })
+   if(!result.success){
+      return{
+         errors: result.error.flatten().fieldErrors
+      }
+   }
+
+
+   return {
+      errors:{}
+   }
+      // revalidate the home page
+
 }
 
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,7 +1,16 @@
+"use client";
 import { Popover, PopoverContent, PopoverTrigger,Button,Input,Textarea } from "@nextui-org/react";
 import * as actions from "@/actions"
+import { useFormState } from "react-dom";
 
 export default function TopicCreateForm(){
+
+// Rememeber useFromState will give you back two things 
+// One is the formState which will have a message 
+// the other thing is the server action 
+
+// it takes in the server action and the intial formstate
+const [formState,action] =  useFormState(actions.createTopic, {errors:{}})
 
     return <div>
       <Popover placement="left">
@@ -9,11 +18,18 @@ export default function TopicCreateForm(){
             <Button color="primary">Create a topic</Button>
         </PopoverTrigger>
         <PopoverContent>
-            <form action={actions.createTopic}>
+            <form action={action}>
                 <div className="flexflex-col gap-4 p-4 w-80">
                     <h3>Create a topic</h3>
-                    <Input name="name" label="Name" labelPlacement="outside" placeholder="Name"/>
-                    <Textarea name="description" label="Description" labelPlacement="outside" placeholder="Describe your topic"/>
+                    <Input name="name" label="Name" labelPlacement="outside" placeholder="Name"
+                    isInvalid={!!formState.errors.name}
+                    errorMessage={formState.errors.name?.join(",")}
+                    />
+                    
+                    <Textarea name="description" label="Description" labelPlacement="outside" placeholder="Describe your topic"
+                    isInvalid={!!formState.errors.description}
+                    errorMessage={formState.errors.description?.join(", ")}
+                    />
                     <Button type="submit">
                         Submit
                     </Button>
